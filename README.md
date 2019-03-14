064a5eb700795429ca165ad620059810301b9e25
Orionn125 <Luka.fila125@gmail.com>
2019-03-14 13:25:06 +0100

    First commit
    First commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-GallerySystem
+ImageGallery
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Instructions ---->
+// Replace configPlaceholder with your firebase credentials
+import { environment } from '../environments/environment';
+import { DropzoneDirective } from './dropzone.directive';
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
+
+
+const config = {
+  apiKey: "AIzaSyAbW1I3_5YI2x4BVONTXaLWB9oDLZjmqws",
+  authDomain: "gallery-51f6c.firebaseapp.com",
+  databaseURL: "https://gallery-51f6c.firebaseio.com",
+  projectId: "gallery-51f6c",
+  storageBucket: "gallery-51f6c.appspot.com",
+  messagingSenderId: "749357181325"
+};
+
+
+@NgModule({
+  declarations: [AppComponent, DropzoneDirective, UploaderComponent, UploadTaskComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
